bcfa3ea966c45d3b60d4952782f476c3906f6848
Pranshu Gupta <pranshumg@gmail.com>
2025-04-28 03:31:05 +0530

    commit

---
+++ functions/average.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float average(float, float, float);
+
+float average(float a, float b, float c)
+{
+	return (a + b + c) / 3;
+}
+
+int main()
+{
+	float x, y, z;
+	printf("enter 1st number : ");
+	scanf("%f", &x);
+	printf("enter 2nd number : ");
+	scanf("%f", &y);
+	printf("enter 3rd number : ");
+	scanf("%f", &z);
+
+	printf("average : %f\n", average(x, y, z));
+
+	return 0;
+}
