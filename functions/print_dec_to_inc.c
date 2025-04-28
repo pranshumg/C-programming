f02deed31b841ef2190f2ba3b3aa033b283c2574
Pranshu Gupta <pranshumg@gmail.com>
2025-04-28 18:34:49 +0530

    commit

---
+++ functions/print_dec_to_inc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void dec_to_inc(int n) {
+
+	printf("%d ", n);
+	if (n == 0) {
+		return;
+	} else {
+		dec_to_inc(n - 1);
+		printf("%d ", n);
+	}
+
+}
+
+int main() {
+
+	int n;
+	printf("enter number = ");
+	scanf("%d", &n);
+
+	dec_to_inc(n);
+	printf("\n");
+
+	return 0;
+}
