1749579ad3fdc4ded253ac93912948a278c320ee
Pranshu Gupta <pranshumg@gmail.com>
2025-04-28 18:57:08 +0530

    commit

---
+++ functions/print_n_to_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void print_n_to_1(int n) {
+
+	if (n == 0) {
+		return;
+	} else {
+		printf("%d ", n);
+		return print_n_to_1(n - 1);
+	}
+
+}
+
+int main() {
+
+	int n;
+	printf("enter number = ");
+	scanf("%d", &n);
+
+	print_n_to_1(n);
+	printf("\n");
+
+	return 0;
+}
