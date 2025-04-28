a2d8dadc8e79fc413667ff5012376d63366227ff
Pranshu Gupta <pranshumg@gmail.com>
2025-04-28 19:05:00 +0530

    commit

---
+++ functions/permutation.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int fact(int n) {
+
+	if (n == 0 || n == 1) {
+		return 1;
+	} else {
+		return n * fact(n - 1);
+	}
+	
+}
+
+void perm(int n, int r) {
+
+	if (r > n) {
+		printf("Invalid input. r cannot be greater than n.\n");
+	} else {
+		int nPr = fact(n) / fact(n - r);
+		printf("nPr with n = %d and r = %d = %d\n", n, r, nPr);
+	}
+
+}
+
+int main() {
+
+	int n, r;
+	printf("enter n = ");
+	scanf("%d", &n);
+	printf("enter r = ");
+	scanf("%d", &r);
+
+	perm(n, r);
+
+	return 0;
+
+}
