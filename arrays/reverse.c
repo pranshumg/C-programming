eba5475eeabedac5cad65d9c6ee6cc37f06018f2
Pranshu Gupta <pranshumg@gmail.com>
2025-04-28 18:13:39 +0530

    commit

--- arrays/reverse.c
+++ arrays/reverse.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 
+void traverse(int arr[], int size) {
+
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void reverse(int arr[], int size) {
+
+	int i = 0, j = size - 1;
+	while (i < j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		i++;
+		j--;
+	}
+}
+
 int main() {
 
-	int n;
-	printf("enter no. of elements = ");
-	scanf("%d", &n);
+	int arr[] = {4, 1, 5, 2, 3,};
+	int size = sizeof(arr) / sizeof(int);
 
-	int arr[n];
+	printf("array = ");
+	traverse(arr, size);
 
-	for (int i = 0; i < n; i++) {
-		printf("enter element %d : ",i + 1);
-		scanf("%d", &arr[i]);
-	}
+	reverse(arr, size);
 
-	for (int i = n - 1; i >= 0; i--) {
-		printf("\narr[%d] : %d", i, arr[i]);
-	}
-
-	printf("\n");
+	printf("reverse of array = ");
+	traverse(arr, size);
 
 	return 0;
 }
