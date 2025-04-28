215b830cb34025300fa06093242d92f0b5de92ae
Pranshu Gupta <pranshumg@gmail.com>
2025-04-28 19:06:07 +0530

    commit

---
+++ functions/power_calc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int a, int b) {
+	
+	if (b == 0) {
+		return 1;
+	} else {
+		return a * power(a, b - 1);
+	}
+
+}
+
+int main() {
+
+	int a, b;
+	printf("enter base = ");
+	scanf("%d", &a);
+	printf("enter exponent = ");
+	scanf("%d", &b);
+
+	printf("%d raised to the power %d = %d\n", a, b, power(a, b));
+
+	return 0;
+}
